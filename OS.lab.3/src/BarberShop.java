e9d8f740c4ddc1364c057531af8411711a7277c3
Seliun Ignat <seliunignat@gmail.com>
2021-11-28 15:56:18 +0300

    Changes in BarberShop

--- OS.lab.3/src/BarberShop.java
+++ OS.lab.3/src/BarberShop.java
@@ -1,2 +1,25 @@
 public class BarberShop {
+
+    private int MAX_VISITORS = 5;
+    private int numberOfVisitors = 5;
+
+    public int getMAX_VISITORS() {
+        return MAX_VISITORS;
+    }
+
+    public void setMAX_VISITORS(int MAX_VISITORS) {
+        this.MAX_VISITORS = MAX_VISITORS;
+    }
+
+    public int getNumberOfVisitors() {
+        return numberOfVisitors;
+    }
+
+    public void setNumberOfVisitors(int numberOfVisitors) {
+        this.numberOfVisitors = numberOfVisitors;
+    }
+
+    public boolean isFull() {
+        return numberOfVisitors >= MAX_VISITORS;
+    }
 }
